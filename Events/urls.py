43ca7ea0b77df96d8f32a2d477cbb02aa7f7e64c
Krishna2864 <75547302+Krishna2864@users.noreply.github.com>
2021-12-24 12:15:27 +0530

    some bug fixes

--- Events/urls.py
+++ Events/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r'admin', views.AdvertisementManagementViewset)
+router.register(r'admin', views.AdminManagementViewset)
 router.register(r'user', views.User_ManagementViewset)
 
 router.register(r'vehicle', views.VehicleManagementViewset,basename='vehicle')
